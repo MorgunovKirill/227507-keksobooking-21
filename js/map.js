2d5ce20b07a84eb78084c02311904c26c7209223
Kirill <kirillmorgunov@mail.ru>
2020-10-20 13:03:00 +0300

    добавляет показ карточки

--- js/map.js
+++ js/map.js
@@ -5,6 +5,12 @@
   const PIN_HEIGHT = 62;
   const PIN_POINTER_HEIGHT = 22;
   const MAX_PINS_TO_SHOW = 5;
+  const PLACE_TYPES = {
+    flat: `Квартира`,
+    bungalow: `Бунгало`,
+    house: `Дом`,
+    palace: `Дворец`
+  };
 
 
   const pin = document.querySelector(`.map__pin--main`);
@@ -39,20 +45,81 @@
     return placeElement;
   };
 
-  const createCard = (arr) => {
-    let card = cardTemplate.cloneNode(true);
-    let cardPhotos = card.querySelector(`.popup__photos`);
+  const checkData = (data, element) => {
+    if (!data.length) {
+      element.remove();
+      return false;
+    }
+    return true;
+  };
 
-    card.querySelector(`.popup__title`).textContent = arr[0].offer.title;
-    card.querySelector(`.popup__text--address`).textContent = arr[0].offer.address;
-    card.querySelector(`.popup__text--price`).textContent = `${arr[0].offer.price} ₽/ночь`;
-    card.querySelector(`.popup__type`).textContent = `${arr[0].offer.type}`;
-    card.querySelector(`.popup__text--capacity`).textContent = `${arr[0].offer.rooms} комнаты для ${arr[0].offer.guests} гостей`;
-    card.querySelector(` .popup__text--time`).textContent = `Заезд после ${arr[0].offer.checkin}, выезд до ${arr[0].offer.checkout}`;
-    // card.querySelector(`.popup__features`).textContent = `${arr[0].offer.features.join(` `)}`;
-    card.querySelector(`.popup__description`).textContent = arr[0].offer.description;
-    // card.querySelector(`.popup__photos`).textContent = `${arr[0].offer.photos.join(` `)}`;
-    card.querySelector(`.popup__avatar`).src = arr[0].author.avatar;
+  const createCard = (arr) => {
+    const card = cardTemplate.cloneNode(true);
+    const cardPhotos = card.querySelector(`.popup__photos`);
+    const cardFeatures = card.querySelector(`.popup__features`);
+    const photoTemplate = cardPhotos.querySelector(`.popup__photo`);
+    const features = arr[0].offer.features;
+
+    cardPhotos.innerHTML = ``;
+
+    if (checkData(arr[0].offer.title, card.querySelector(`.popup__title`))) {
+      card.querySelector(`.popup__title`).textContent = arr[0].offer.title;
+    }
+
+    if (checkData(arr[0].offer.address, card.querySelector(`.popup__text--address`))) {
+      card.querySelector(`.popup__text--address`).textContent = arr[0].offer.address;
+    }
+
+    if (checkData(arr[0].offer.price, card.querySelector(`.popup__text--price`))) {
+      card.querySelector(`.popup__text--price`).textContent = `${arr[0].offer.price} ₽/ночь`;
+    }
+
+
+    if (checkData(arr[0].offer.type, card.querySelector(`.popup__type`))) {
+      card.querySelector(`.popup__type`).textContent = PLACE_TYPES[arr[0].offer.type];
+    }
+
+    if (checkData(arr[0].offer.rooms, card.querySelector(`.popup__text--capacity`))) {
+      if (arr[0].offer.guests) {
+        card.querySelector(`.popup__text--capacity`).textContent = `${arr[0].offer.rooms} комнаты для ${arr[0].offer.guests} гостей`;
+      } else {
+        card.querySelector(`.popup__text--capacity`).remove();
+      }
+    }
+
+    if (checkData(arr[0].offer.checkin, card.querySelector(`.popup__text--time`))) {
+      if (arr[0].offer.checkout) {
+        card.querySelector(`.popup__text--time`).textContent = `Заезд после ${arr[0].offer.checkin}, выезд до ${arr[0].offer.checkout}`;
+      } else {
+        card.querySelector(`.popup__text--time`).remove();
+      }
+    }
+
+    if (checkData(features, cardFeatures)) {
+      for (let i = 0; i < features.length; i++) {
+        cardFeatures.querySelectorAll(`.popup__feature`).forEach(function (element) {
+          if (element.classList.contains(`popup__feature` + `--${features[i]}`)) {
+            element.style.display = `inline-block`;
+          }
+        });
+      }
+    }
+
+    if (checkData(arr[0].offer.description, card.querySelector(`.popup__description`))) {
+      card.querySelector(`.popup__description`).textContent = arr[0].offer.description;
+    }
+
+    if (checkData(arr[0].offer.photos, cardPhotos)) {
+      arr[0].offer.photos.forEach(function (element) {
+        let newPhoto = photoTemplate.cloneNode(true);
+        newPhoto.setAttribute(`src`, element);
+        cardPhotos.appendChild(newPhoto);
+      });
+    }
+
+    if (checkData(arr[0].author.avatar, card.querySelector(`.popup__avatar`))) {
+      card.querySelector(`.popup__avatar`).src = arr[0].author.avatar;
+    }
 
     return card;
   };
